9902ec04118e62a2d5e59144d34a9a06e9159405
pando <spreadpando@gmail.com>
2019-04-23 21:56:53 -0600

    this is fucked

--- assets/index.js
+++ assets/index.js
@@ -5,15 +5,30 @@ function addButton() {
 	let newButton = $("#add-button").val().trim();
 	topics.push(newButton);
 	renderButtons();
+	$('#add-button').val('');
 }
+$(document).on('click', '.btn', addGiphs);
 
-function addGiphs(clicked) {
-	let query = clicked.class();
+function addGiphs() {
+	let query = $(this).attr('data-name');
+	console.log(query);
 	let queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=qntGR5Te1E7CLsmv81lfvDLbSeA2HiiO&q=' + query + '&limit=25&offset=0&rating=G&lang=en';
 	$.ajax({
 		url: queryURL,
 		method: 'GET'
 	}).then(function (response) {
+		let result = response.data;
+		console.log(result[0].images.fixed_height_still.url);
+		for (var i; i < result.length; i++) {
+			var div = $('<div class="gif">');
+			console.log(div);
+			var img = $('<img class="gif-image">');
+			var still = result[i].images.fixed_height_still.url;
+			var anim = result[i].images.fixed_height.url;
+			img.attr('src', still);
+			div.append(img);
+			$('#gifs').prepend(div);
+		}
 
 	});
 
@@ -23,12 +38,11 @@ function renderButtons() {
 	$('#buttons').html('');
 	for (var i in topics) {
 		let button = $('<button>');
-		button.addClass('topics[i]');
+		button.attr('data-name', topics[i]);
+		button.attr('class', 'btn');
 		button.text(topics[i]);
 		$('#buttons').append(button);
-		$('.' + topics[i]).on('click', function () {
-			addGiphs(this);
-		})
+
 	}
 }
 
